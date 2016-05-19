a8d7dffedab0814bbeb2ffcaa427a063f9b26851
t1t1 <cha@hanmail2.net>
2016-05-19 15:21:20 +0900

    db config

---
+++ spring-mvc-jdbc/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into emp(ename, sal) values('kim', 1000);
+insert into emp(ename, sal) values('lee', 2000);
+insert into emp(ename, sal) values('cha', 3000);
---
+++ spring-mvc-jdbc/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+drop database if exists mvc1;
+create datavase mvc1;
+use mvc1;
+create table emp
+(
+	empno int(4) not null auto_increment,
+	ename varchar(50),
+	sal int(4),
+	primary key (empno)
+) ENGINE=InnoDB;
